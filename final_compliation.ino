8496a2a862f145e711bfcf42353880318b3f5b58
einew-dinesh <57245685+einew-dinesh@users.noreply.github.com>
2023-05-09 16:26:28 +0530

    Add files via upload
    Final esp8266 Code

--- final_compliation.ino
+++ final_compliation.ino
@@ -29,14 +29,17 @@ FirebaseJsonArray arr;
 unsigned long sendDataPrevMillis = 0;
 bool signupOK = false;
 
-const int samples = 512;
+const int samples = 1024;
 int samplingFrequency = 493;
 unsigned long StartTime;
 
 double xReal[samples];
 double xImag[samples];
-//float yss[512];
-//float zss[512];
+//double yReal[samples];
+//double yImag[samples];
+//double zReal[samples];
+//double zImag[samples];
+
 
 
 
@@ -53,13 +56,19 @@ void setup() {
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+//    for(int j=0;j<10;j++){
+//      digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+//      delay(200);                      // Wait for a second
+//      digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+//      delay(200); 
+//    }
+     
     while(1);
   }
 
   /* Set the range to whatever is appropriate for your project */
-  accel.setRange(ADXL345_RANGE_4_G);
-  accel.setDataRate(ADXL345_DATARATE_400_HZ);
-  /* Display some basic information on this sensor */
+  accel.setRange(ADXL345_RANGE_8_G);
+  
 
    
   /////////////////////////////////////////////////////
@@ -104,19 +113,18 @@ void loop() {
     accel.getEvent(&event);
 
       xReal[i]=event.acceleration.x;
+      
       xImag[i] = 0.0;
-      //yss[i]=event.acceleration.y;
-      //zss[i]=event.acceleration.z;
+      
       i=i+1;
-      if(millis()-StartTime==1000){
-        samplingFrequency=i;
-        
-      }
+//      if(millis()-StartTime==1000){
+//        samplingFrequency=i;
+//        
+//      }
     
   }
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.print("Sampling Frequency :-> ");Serial.println(samplingFrequency);
-  delay(1000);
+  Serial.print("Sampling Frequency :-> ");Serial.print(",");Serial.println(samplingFrequency);
   
 //  for ( int m = 0; m < samples; m = m + 1) {
 //    Serial.print(m);Serial.print("  ");Serial.print("     X: "); Serial.println(xReal[m]);
@@ -129,6 +137,16 @@ void loop() {
   FFT.Windowing(xReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
   FFT.Compute(xReal, xImag, samples, FFT_FORWARD); /* Compute FFT */
   FFT.ComplexToMagnitude(xReal, xImag, samples); /* Compute magnitudes */
+
+//  FFT.Windowing(yReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
+//  FFT.Compute(yReal, xImag, samples, FFT_FORWARD); /* Compute FFT */
+//  FFT.ComplexToMagnitude(yReal, xImag, samples); /* Compute magnitudes */
+//
+//  FFT.Windowing(zReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
+//  FFT.Compute(zReal, xImag, samples, FFT_FORWARD); /* Compute FFT */
+//  FFT.ComplexToMagnitude(zReal, xImag, samples); /* Compute magnitudes */
+  Serial.println("/////////// ");
+  
   //PrintVector(xReal, (samples >> 1), SCL_FREQUENCY);
   //double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
   //Serial.println(x, 6);
@@ -161,7 +179,6 @@ void loop() {
       json.add("Sampling Frequency", samplingFrequency);
       json.add("Samples", samples);
       Serial.printf("Sending Sampling Frequency %s\n", Firebase.RTDB.set(&fbdo, "fft/fft_meta", &json) ? "ok" : fbdo.errorReason().c_str());
-      delay(2000);
       json.clear();
      
 //     if(Firebase.RTDB.updateNode(&fbdo, "fft/fft_samplingFrequency", &json)){
@@ -186,30 +203,44 @@ void loop() {
       for(i=0;i<100;i++){
         arr.add(xReal[i]);
       }
-      
-    Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f1", &arr) ? "ok" : fbdo.errorReason().c_str());
-     arr.clear();
+      Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f1", &arr) ? "ok" : fbdo.errorReason().c_str());
+      arr.clear();
 
- for(i=100;i<200;i++){
+      for(i=100;i<200;i++){
+        arr.add(xReal[i]);
+      }
+      Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f2", &arr) ? "ok" : fbdo.errorReason().c_str());
+      arr.clear();
+
+      for(i=200;i<300;i++){
+        arr.add(xReal[i]);
+      }
+      Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f3", &arr) ? "ok" : fbdo.errorReason().c_str());
+      arr.clear();
+
+      for(i=300;i<400;i++){
+        arr.add(xReal[i]);
+      }
+      Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f4", &arr) ? "ok" : fbdo.errorReason().c_str());
+       arr.clear();
+       
+      for(i=400;i<500;i++){
+        arr.add(xReal[i]);
+      }
+      Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f5", &arr) ? "ok" : fbdo.errorReason().c_str());
+      arr.clear();
+      
+      for(i=500;i<512;i++){
         arr.add(xReal[i]);
       }
       
-    Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f2", &arr) ? "ok" : fbdo.errorReason().c_str());
- arr.clear();
-
- for(i=200;i<256;i++){
-        arr.add(xReal[i]);
-      }
-      
-    Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f3", &arr) ? "ok" : fbdo.errorReason().c_str());
-
+    Serial.printf("Set array... %s\n", Firebase.RTDB.setArray(&fbdo, "fft/fft_arr/f6", &arr) ? "ok" : fbdo.errorReason().c_str());
         arr.clear();
     
      
   }
   
 Serial.println("////////////");
-delay(10000);
 
 }
 
@@ -238,7 +269,7 @@ void PrintVector(double *vData, uint16_t bufferSize, uint8_t scaleType)
     Serial.print(abscissa, 6);
     if(scaleType==SCL_FREQUENCY)
       Serial.print(" Hz");
-    Serial.print("  ");
+    Serial.print(",");
     Serial.println(vData[i], 4);
   }
   Serial.println();
